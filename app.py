305a531754260ae2a56f2a666030a2643aecd2d3
seanlzx <seanleezhxi@gmail.com>
2022-11-08 11:22:20 +0800

    modified:   __pycache__/app.cpython-311.pyc modified:   app.py modified:   scripts/__pycache__/helpers.cpython-311.pyc modified:   scripts/helpers.py new file:   scripts/nutrition.db modified:   templates/index.html

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-import sqlite3
 from contextlib import suppress
 from os import supports_bytes_environ
 
 from flask import Flask, render_template, request
 
-from scripts.helpers import getFormListValues
+from scripts.helpers import *
+
+DATABASE = 'nutrition.db'
 
 app = Flask(__name__)
 
 # development variables, should eventually get rid of the hardcode
 userid = 0
 driGroupName = 'male 19-30'
+
+@app.teardown_appcontext
+def close_connectioncall(exception):
+    close_connection(exception)
 
 @app.route("/addFood", methods=["POST", "GET"])
 def addFood():
@@ -62,6 +67,7 @@ def addFood():
             "cholestrol",
             "creatine",
             "caffeine",
+            "good_bacteria",
             "mercury",
             "nitrates_and_nitrites",
             "Artificial_food_colors",
@@ -79,4 +85,24 @@ def addFood():
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    c = get_db().cursor()
+    
+    raw_category = c.execute('SELECT name, id FROM category')
+    category_dict = listOfTuplesToListOfDict(raw_category, ['name','id'])
+    
+    raw_ctp = c.execute('SELECT p.id AS p_id, p.name AS p_name, c.id AS c_id, c.name AS c_name FROM category p, category c, category_to_parent ctp WHERE p.id = ctp.parent_id AND c.id = ctp.child_id;')
+    ctp_dict = listOfTuplesToListOfDict(raw_ctp, ['parent_id', 'parent_name', 'child_id', 'child_name']) 
+    
+    # for child category get parent
+    for category in category_dict:
+        print(f"child: {category['name']}", end=' ')
+        for ctp in ctp_dict:
+            if ctp['child_id'] == category['id']:
+                print(f"\tparent: {ctp['parent_name']}",end=' ')
+        print('')        
+
+    # find the best way to arrange the categories
+    
+    return render_template("index.html", category_dict=category_dict)
+
+
--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,5 +1,13 @@
-from flask import request
+import sqlite3
+from os import path
+from pathlib import Path
 
+from flask import g, request
+
+parent = Path(path.dirname(path.abspath(__file__))).parent.absolute()
+DATABASE = path.join(parent,"nutrition.db")
+
+print(DATABASE)
 
 def getFormListValues(list):
     dict = {}
@@ -7,6 +15,31 @@ def getFormListValues(list):
         value = request.form.get(str)
         if value:
             dict[str] = value
-            
+
     return dict
 
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
+
+# ensure the the number of elements in tuple is equal to elements in listOfKey
+def listOfTuplesToListOfDict(listOfTuples, listOfKeys):
+    list = []
+    for tuple in listOfTuples:
+        list.append(tupleToDict(tuple, listOfKeys))
+    return list
+        
+        
+def tupleToDict(tuple, listOfKeys):
+    dict = {}
+    for key, item in zip(listOfKeys, tuple):
+        dict[key] = item
+    return dict
